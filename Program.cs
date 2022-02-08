7672fb4393a9a62f096facdb6844531ff0f956ba
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-08 13:46:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            string lastName;
+            Console.Write("Por favor, insira seu nome: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Agora insira seu sobrenome: ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine(string.Format("Olá, meu nome é {0} {1}.", Name, lastName));
+
+
+        }
+    }
+}
